78582b22d99296ab5a952b94b5833d04638a559c
Ahmed Atwa <a.atwaa94@gmail.com>
2025-11-13 12:15:48 +0200

    bump version

--- result/build.gradle.kts
+++ result/build.gradle.kts
@@ -40,7 +40,7 @@ afterEvaluate {
 
                 groupId = "com.github.atwa"
                 artifactId = "filepicker-result"
-                version = "1.0.0"
+                version = "1.0.0-SNAPSHOT"
             }
         }
         repositories {
